bf4bce2dbb6d47e58bcb0e9f95d834db143f5d4a
Wilson Cheng <wilson.cheng@gmail.com>
2020-02-25 10:20:17 +0800

    fix bug

--- exec-v1-ionic.bat
+++ exec-v1-ionic.bat
@@ -1,4 +1,7 @@
 echo off
+
 for /f %%i in ('docker ps -qf "expose=8100"') do set cid=%%i
-echo %cid% 
+
+echo Start zsh in contaier %cid%, run 'ionic serve --external" to expose ionic to windows
+
 docker exec -it %cid% zsh
--- run-v1-ionic.bat
+++ run-v1-ionic.bat
@@ -1,3 +1,5 @@
-echo run $ ionic serve --external 
+echo off
+
+echo Start docker image in detach mode, run exec-v1-ionic.bat to log into the container once done.
 
 docker run --rm -d -it -p 8100:8100 -p 8080:8080 -p 30000:30000 -p 35729:35729 -p 53703:53703 -v "d:\wilson cheng\dockerdir":/hostdir ubuntu-dev-v1-ionic
